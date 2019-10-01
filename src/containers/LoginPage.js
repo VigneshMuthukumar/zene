3f7a0d6cdf62dfe5ffb0a0fd786dece759a8d399
Vignesh M <vigneshmuthukumar12@gmail.com>
2019-10-01 20:46:56 +0530

    Semantic UI added
    UI library added

--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -2,7 +2,10 @@ import React from "react";
 import { connect } from "react-redux";
 import Login from './../components/Login'
 class LoginPage extends React.Component {
-
+    componentWillMount(){
+        if(localStorage.getItem('access_token')) 
+             this.props.history.push('/') 
+    }
     render(){
         return(
            <Login {...this.props} />
